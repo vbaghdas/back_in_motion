18acf845ba934dde2253a4cefbabce6bbf7099d9
vbaghdas <vbaghdas@gmail.com>
2017-09-03 15:42:31 -0700

    added php email form with captcha validation

---
+++ send_form_email.php
@@ -0,0 +1,99 @@
+<?php
+function post_captcha($user_response) {
+    $fields_string = '';
+    $fields = array(
+        'secret' => '6LfwPC8UAAAAAEfebsHh9c2FKZsPzYz-ZFi2Q7WC',
+        'response' => $user_response
+    );
+    foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+    $fields_string = rtrim($fields_string, '&');
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($result, true);
+}
+
+// Call the function post_captcha
+$res = post_captcha($_POST['g-recaptcha-response']);
+
+if (!$res['success']) {
+// What happens when the CAPTCHA wasn't checked
+    header('Location: error.php');
+    return;
+} else {
+// If CAPTCHA is successfully completed...
+
+// Paste mail function or whatever else you want to happen here!
+    header('Location: thank_you.php');
+}
+
+
+
+// ****The mail function is below**** //
+if(!isset($_POST['submit']))
+{
+    //This page should not be accessed directly. Need to submit the form.
+    echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email))
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'vbaghdas@gmail.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n\r".
+    "Here is the message:\n\r $message". "\n\r to: \n\r" .
+
+    $to = "vbaghdas@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank_you.php');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+        '(\r+)',
+        '(\t+)',
+        '(%0A+)',
+        '(%0D+)',
+        '(%08+)',
+        '(%09+)'
+    );
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    if(preg_match($inject,$str))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+?>
